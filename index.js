331ddada4cfdfa602f2c7a842295c432a3e193c5
aalvareztr <aalvarez@wecom.gobal>
2024-01-19 10:20:10 -0300

    se

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import AppRoutes from './router/AppRouter.js';
 import cors from 'cors';
 import 'dotenv/config';
 import cookieParser from "cookie-parser";
-
+import bcrypt from 'bcrypt';
 
 const app = express()
 const corsOptions = {
@@ -17,3 +17,5 @@ app.use(cookieParser())
 app.use(AppRoutes);
 const PORT = process.env.PORT || 3000
 app.listen(process.env.PORT || 3000);
+
+
